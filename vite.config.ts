72f240803953f9328aee642b62b69777df8bb2f6
44iiToomp44ka <toomp4@gmail.com>
2024-05-12 16:46:01 +0500

    wawa

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/toomp4/",
   build: {
     outDir: 'dist'
   },
   // @ts-ignore
-  base: process.env.GH_PAGES ? '/hackatonantikor/' : './',
-  server: {
-    fs: {
-      allow: ['../sdk', './'],
-    },
-  },
 })
